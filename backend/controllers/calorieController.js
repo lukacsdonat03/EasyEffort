7c97457ed7c498daec77e3b1b903cc5309e27c7a
lukacsdonat03 <lukacsdonatxxx@gmail.com>
2023-03-09 19:20:08 +0100

    calorieController fix apinak megfelelően

--- backend/controllers/calorieController.js
+++ backend/controllers/calorieController.js
@@ -18,7 +18,7 @@ const deleteItem = async (req, res) => {
 
 const createItem = async (req, res) => {
   const product = { ...req.body };
-  if (!product.id || !product.index) {
+  if (!product.item_id) {
     res
       .status(StatusCodes.NOT_ACCEPTABLE)
       .send("Nincs termék ezzel az azonosítóval");
@@ -26,36 +26,35 @@ const createItem = async (req, res) => {
   database.query(
     "INSERT INTO calorie VALUES(?,?,?,?,?,?,?)",
     [
-      product.name,
+      product.item_name,
       product.amount,
       product.carbohydrate,
       product.protein,
       product.fat,
-      product.category,
       product.userId,
     ],
     (err) => {
       if (err) {
         res.status(StatusCodes.NOT_FOUND).send(err);
       }
-      res.status(StatusCodes.OK).send({ product: product.name });
+      res.status(StatusCodes.OK).send({ product: product.item_name,id:item_id });
     }
   );
 };
 
 const updateItem = (req, res) => {
   const product = { ...req.body };
-  if (!product.id) throw new NotFoundError("Nincs termék ilyen id-vel");
+  if (!product.item_id) throw new NotFoundError("Nincs termék ilyen id-vel");
   database.query(
     "UPDATE calorie SET name = ?, amount = ? WHERE id = ? ",
-    [product.name, product.amount,product.id],
+    [product.item_name, product.amount,product.item_id],
     (err) => {
       if (err) {
         res.status(StatusCodes.NOT_FOUND).send(err);
       }
       res
         .status(StatusCodes.OK)
-        .send({ product: product.name, amount: product.amount });
+        .send('Updated \n'+{id:item_id, product: product.item_name, amount: product.amount });
     }
   );
 };
@@ -70,7 +69,7 @@ const allItem = (req, res)=>{
 }
 
 const getItem = (req, res)=>{
-  const {id} = req.params.id
+  const id = req.params.item_id
   if(!id) throw new BadRequestError('Nincs id')
   database.query('SELECT * FROM calorie WHERE id = ?',[id],(err,rows)=>{
     if(err){
