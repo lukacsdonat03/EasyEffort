4e528d6d71946d2d749ebb25f4b078d9b8782ff8
Lukács Donát <lukacsdonatxxx@gmail.com>
2023-03-11 18:44:40 +0100

    styling and testing frontend

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import{BrowserRouter, Route, Routes} from 'react-router-dom'
 import './App.css';
+import { TestList } from './components/TestList';
 import { CalorieCounter } from './pages/CalorieCounter';
 import { Login } from './pages/Login';
 import { Register } from './pages/Register';
@@ -14,6 +15,7 @@ function App() {
          <Route path='/login' element={<Login />}/>
          <Route path='/register' element={<Register />}/>
          <Route path='/counter' element={<CalorieCounter />} />
+         <Route path='/test' element={<TestList/>} />
         </Routes>
       </BrowserRouter>
     </>  
