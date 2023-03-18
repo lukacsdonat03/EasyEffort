c2c46845d5b3419272628616c5e0ee24d8957950
lukacsdonat03 <lukacsdonatxxx@gmail.com>
2023-03-18 15:06:50 +0100

    kalória számláló fix backend és frontend

--- backend/controllers/calorieController.js
+++ backend/controllers/calorieController.js
@@ -19,48 +19,58 @@ const deleteItem = async (req, res) => {
 const createItem = (req, res) => {
   const product = { ...req.body };
   console.log(product);
-  database.query('INSERT INTO calorie (name,amount,carbohydrate,protein,fat,totalCalorie,userId) VALUES(?,?,?,?,?,?,?);',[product.name, product.amount,product.carbohydrate,product.protein,product.fat,product.totalCalorie,product.userId],(err)=>{
-    if(err) return res.status(StatusCodes.INTERNAL_SERVER_ERROR).send(err)
-    res.status(StatusCodes.CREATED).send('Created...')
-  })
+  database.query('INSERT INTO calorie (name,amount,carbohydrate,protein,fat,totalCalorie,userId) VALUES(?,?,?,?,?,?,?);',
+    [
+      product.name * product.amount, 
+      product.amount, 
+      product.carbohydrate * product.amount,
+       product.protein * product.amount,
+        product.fat * product.amount,
+         product.totalCalorie * product.amount,
+          product.userId
+        ],
+         (err) => {
+      if (err) return res.status(StatusCodes.INTERNAL_SERVER_ERROR).send(err)
+      res.status(StatusCodes.CREATED).send('Created...')
+    })
 };
 
 const updateItem = (req, res) => {
   const id = req.params.id
-  const {item_name,amount} = req.body;
-  if (!id)  return res.status(StatusCodes.BAD_REQUEST).send('Nics termék azonosító!')
+  const { item_name, amount } = req.body;
+  if (!id) return res.status(StatusCodes.BAD_REQUEST).send('Nics termék azonosító!')
   database.query(
     "UPDATE calorie SET name = ?, amount = ? WHERE id = ? ",
-    [item_name, amount,id],
+    [item_name, amount, id],
     (err) => {
       if (err) {
         res.status(StatusCodes.NOT_FOUND).send(err);
       }
       res
         .status(StatusCodes.OK)
-        .send('Updated \n'+{id: id, name: item_name,amount: amount });
+        .send('Updated \n' + { id: id, name: item_name, amount: amount });
     }
   );
 };
 
-const allItem = (req, res)=>{
+const allItem = (req, res) => {
   const userId = req.params.userId
-  database.query('SELECT * FROM calorie WHERE userId=?;',[userId],(err,rows)=>{
-    if(err){
+  database.query('SELECT * FROM calorie WHERE userId=?;', [userId], (err, rows) => {
+    if (err) {
       res.status(StatusCodes.INTERNAL_SERVER_ERROR).send(err)
     }
     res.status(StatusCodes.OK).send(rows)
   })
 }
 
-const getItem = (req, res)=>{
+const getItem = (req, res) => {
   const id = req.params.id
-  if(!id) throw new BadRequestError('Nincs id')
-  database.query('SELECT * FROM calorie WHERE id = ?',[id],(err,rows)=>{
-    if(err){
+  if (!id) throw new BadRequestError('Nincs id')
+  database.query('SELECT * FROM calorie WHERE id = ?', [id], (err, rows) => {
+    if (err) {
       res.status(StatusCodes.INTERNAL_SERVER_ERROR).send(err)
     }
-    if(rows.length === 0){
+    if (rows.length === 0) {
       throw new NotFoundError(`No item with ${id} id`)
     }
     res.status(StatusCodes.OK).send(rows[0])
--- client/src/components/Counter.jsx
+++ client/src/components/Counter.jsx
@@ -3,45 +3,72 @@ import Grid from "@mui/material/Grid";
 import { Autocomplete, Button, TextField } from "@mui/material";
 import axios from "axios";
 
-
-let PRODUCTS = []
+let PRODUCTS = [];
 
 export const Counter = () => {
   const [input, setInput] = useState("");
-  const [search,setSearch] = useState("%20");
-  const [ selectedItem, setSelectedItem ] = useState({})
+  const [search, setSearch] = useState("%20");
+  const [selectedItem, setSelectedItem] = useState({});
 
-  useEffect(()=>{
+  /*useEffect(()=>{
     axios.post('http://localhost:8080/api/v1/counter/items',{item_name:search},{headers:{'Content-Type':'application/json'},withCredentials:true})
       .then(res => {
         res.data.map((element,index)=>PRODUCTS.push({id:index+1,label:element.item_name,fields:element}))
         console.log(PRODUCTS); 
       })
 
-  },[search])
+  },[search])*/
 
-  
+  const handleInput = (e) => {
+    axios
+      .post(
+        "http://localhost:8080/api/v1/counter/items",
+        { item_name: search },
+        {
+          headers: { "Content-Type": "application/json" },
+          withCredentials: true,
+        }
+      )
+      .then((res) => {
+        res.data.map((element, index) =>
+          PRODUCTS.push({
+            id: index + 1,
+            label: element.item_name,
+            fields: element,
+          })
+        );
+        console.log(PRODUCTS);
+        console.log(search);
+      });
+  };
 
   const handleClick = (e) => {
     setInput(input + e.target.value);
   };
   const handleSubmit = (e) => {
     //e.preventDefeult();
-    setInput('');
+    setInput("");
     const item = selectedItem.fields;
-    axios.post('http://localhost:8080/api/v1/products/',
-    {name:item.item_name,amount:item.nf_serving_size_qty * input ,carbohydrate: item.nf_total_carbohydrate,protein:item.nf_protein,fat:item.nf_total_fat,totalCalorie:item.nf_calories,userId:12},
-    {headers:{'Content-Type':'application/json'},withCredentials:true})
+    axios.post(
+      "http://localhost:8080/api/v1/products/",
+      {
+        name: item.item_name,
+        amount: item.nf_serving_size_qty * input,
+        carbohydrate: item.nf_total_carbohydrate,
+        protein: item.nf_protein,
+        fat: item.nf_total_fat,
+        totalCalorie: item.nf_calories,
+        userId: 12,
+      },
+      { headers: { "Content-Type": "application/json" }, withCredentials: true }
+    );
   };
-  const handleSearch = (e)=>{
-    if(e.keyCode === 13){
-      PRODUCTS.splice(0,PRODUCTS.length)
-      setSearch(e.target.value)
+  const handleSearch = (e) => {
+      PRODUCTS.splice(0, PRODUCTS.length);
+      setSearch(e.target.value);
       console.log(search);
       console.log(Object.keys(PRODUCTS));
-    }
-  }
-  
+  };
 
   return (
     <div className="counter-box">
@@ -49,24 +76,38 @@ export const Counter = () => {
         <Grid container spacing={1}>
           <Grid item xs={12}>
             <Autocomplete
-              style={{width: "100%"}}
+              style={{ width: "100%" }}
               disablePortal
               id="combo-box-demo"
               //className="fullwidth" nem irja felul ://
               options={PRODUCTS}
-              renderOption={(props,option)=>{
-                return(
-                  <li {...props} key={option.id} >{option.label}</li> 
-                )
+              renderOption={(props, option) => {
+                return (
+                  <li {...props} key={option.id}>
+                    {option.label}
+                  </li>
+                );
               }}
               sx={{ width: 300 }}
-              onChange={(event,value)=>{setSelectedItem(value);console.log(value.fields);}}
+              onChange={(event, value) => {
+                setSelectedItem(value);
+                console.log(value.fields);
+              }}
               renderInput={(params) => (
-                <TextField {...params} label="Search..."  onKeyDown={handleSearch}/>
+                <TextField
+                  {...params}
+                  label="Search..."
+                  onChange={handleSearch}
+                />
               )}
-              />
-             
+            />
             <br />
+            <Button variant="contained" color="primary" className="counter-button" onClick={handleInput}>
+              Search:
+            </Button>
+            <br />
+          </Grid>
+          <Grid item xs={12}>
             <input
               className="fullwidth border-radius"
               type="text"
@@ -77,12 +118,17 @@ export const Counter = () => {
           </Grid>
 
           <Grid item xs={4}>
-            <Button  value={1} className="counter-button" onClick={handleClick} variant="contained" >
+            <Button
+              value={1}
+              className="counter-button"
+              onClick={handleClick}
+              variant="contained"
+            >
               1
             </Button>
           </Grid>
           <Grid item xs={4}>
-            <Button value={2} onClick={handleClick} variant="contained" >
+            <Button value={2} onClick={handleClick} variant="contained">
               2
             </Button>
           </Grid>
@@ -93,44 +139,44 @@ export const Counter = () => {
           </Grid>
 
           <Grid item xs={4}>
-            <Button value={4} onClick={handleClick} variant="contained" >
+            <Button value={4} onClick={handleClick} variant="contained">
               4
             </Button>
           </Grid>
           <Grid item xs={4}>
-            <Button value={5} onClick={handleClick} variant="contained" >
+            <Button value={5} onClick={handleClick} variant="contained">
               5
             </Button>
           </Grid>
           <Grid item xs={4}>
-            <Button value={6} onClick={handleClick} variant="contained" >
+            <Button value={6} onClick={handleClick} variant="contained">
               6
             </Button>
           </Grid>
 
           <Grid item xs={4}>
-            <Button value={7} onClick={handleClick} variant="contained" >
+            <Button value={7} onClick={handleClick} variant="contained">
               7
             </Button>
           </Grid>
           <Grid item xs={4}>
-            <Button value={8} onClick={handleClick} variant="contained" >
+            <Button value={8} onClick={handleClick} variant="contained">
               8
             </Button>
           </Grid>
           <Grid item xs={4}>
-            <Button value={9} onClick={handleClick} variant="contained" >
+            <Button value={9} onClick={handleClick} variant="contained">
               9
             </Button>
           </Grid>
 
           <Grid item xs={4}>
-            <Button value={","} onClick={handleClick} variant="contained" > 
+            <Button value={","} onClick={handleClick} variant="contained">
               ,
             </Button>
           </Grid>
           <Grid item xs={4}>
-            <Button value={0} onClick={handleClick} variant="contained" >
+            <Button value={0} onClick={handleClick} variant="contained">
               0
             </Button>
           </Grid>
