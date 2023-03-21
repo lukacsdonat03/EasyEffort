5ff80fe3a2b278d9d20ce698243ff7a0eea29448
Lukács Donát <lukacsdonatxxx@gmail.com>
2023-03-21 09:08:58 +0100

    calorie controlle fix

--- backend/controllers/calorieController.js
+++ backend/controllers/calorieController.js
@@ -1,13 +1,13 @@
 const database = require("../database/dbConfig");
-const {
-  BadRequestError,
-  NotFoundError,
-} = require("../errors");
+const { BadRequestError, NotFoundError } = require("../errors");
 const { StatusCodes } = require("http-status-codes");
 
 const deleteItem = async (req, res) => {
   const { id } = req.params;
-  if (!id) return res.status(StatusCodes.BAD_REQUEST).send("Nincs ezzel az id-vel termék");
+  if (!id)
+    return res
+      .status(StatusCodes.BAD_REQUEST)
+      .send("Nincs ezzel az id-vel termék");
   database.query("DELETE FROM calorie WHERE id = ?;", [id], (err) => {
     if (err) {
       res.status(StatusCodes.NOT_FOUND).send(err);
@@ -19,26 +19,29 @@ const deleteItem = async (req, res) => {
 const createItem = (req, res) => {
   const product = { ...req.body };
   console.log(product);
-  database.query('INSERT INTO calorie (name,amount,carbohydrate,protein,fat,totalCalorie,userId) VALUES(?,?,?,?,?,?,?);',
+  database.query(
+    "INSERT INTO calorie (name,amount,carbohydrate,protein,fat,totalCalorie,userId) VALUES(?,?,?,?,?,?,?);",
     [
-      product.name * product.amount, 
-      product.amount, 
+      product.name,
+      product.amount,
       product.carbohydrate * product.amount,
-       product.protein * product.amount,
-        product.fat * product.amount,
-         product.totalCalorie * product.amount,
-          product.userId
-        ],
-         (err) => {
-      if (err) return res.status(StatusCodes.INTERNAL_SERVER_ERROR).send(err)
-      res.status(StatusCodes.CREATED).send('Created...')
-    })
+      product.protein * product.amount,
+      product.fat * product.amount,
+      product.totalCalorie * product.amount,
+      product.userId,
+    ],
+    (err) => {
+      if (err) return res.status(StatusCodes.INTERNAL_SERVER_ERROR).send(err);
+      res.status(StatusCodes.CREATED).send("Created...");
+    }
+  );
 };
 
 const updateItem = (req, res) => {
-  const id = req.params.id
+  const id = req.params.id;
   const { item_name, amount } = req.body;
-  if (!id) return res.status(StatusCodes.BAD_REQUEST).send('Nics termék azonosító!')
+  if (!id)
+    return res.status(StatusCodes.BAD_REQUEST).send("Nics termék azonosító!");
   database.query(
     "UPDATE calorie SET name = ?, amount = ? WHERE id = ? ",
     [item_name, amount, id],
@@ -48,36 +51,38 @@ const updateItem = (req, res) => {
       }
       res
         .status(StatusCodes.OK)
-        .send('Updated \n' + { id: id, name: item_name, amount: amount });
+        .send("Updated \n" + { id: id, name: item_name, amount: amount });
     }
   );
 };
 
 const allItem = (req, res) => {
-  const userId = req.params.userId
-  database.query('SELECT * FROM calorie WHERE userId=?;', [userId], (err, rows) => {
-    if (err) {
-      res.status(StatusCodes.INTERNAL_SERVER_ERROR).send(err)
+  const userId = req.params.userId;
+  database.query(
+    "SELECT * FROM calorie WHERE userId=?;",
+    [userId],
+    (err, rows) => {
+      if (err) {
+        res.status(StatusCodes.INTERNAL_SERVER_ERROR).send(err);
+      }
+      res.status(StatusCodes.OK).send(rows);
     }
-    res.status(StatusCodes.OK).send(rows)
-  })
-}
+  );
+};
 
 const getItem = (req, res) => {
-  const id = req.params.id
-  if (!id) throw new BadRequestError('Nincs id')
-  database.query('SELECT * FROM calorie WHERE id = ?', [id], (err, rows) => {
+  const id = req.params.id;
+  if (!id) throw new BadRequestError("Nincs id");
+  database.query("SELECT * FROM calorie WHERE id = ?", [id], (err, rows) => {
     if (err) {
-      res.status(StatusCodes.INTERNAL_SERVER_ERROR).send(err)
+      res.status(StatusCodes.INTERNAL_SERVER_ERROR).send(err);
     }
     if (rows.length === 0) {
-      throw new NotFoundError(`No item with ${id} id`)
+      throw new NotFoundError(`No item with ${id} id`);
     }
-    res.status(StatusCodes.OK).send(rows[0])
-  })
-}
-
-
+    res.status(StatusCodes.OK).send(rows[0]);
+  });
+};
 
 module.exports = {
   getItem,
@@ -85,4 +90,4 @@ module.exports = {
   updateItem,
   createItem,
   deleteItem,
-}
+};
