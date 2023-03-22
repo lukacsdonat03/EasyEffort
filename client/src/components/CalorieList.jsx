fc046f5979384c8ff3e412d6c635643261b8fc7b
Lukács Donát <lukacsdonatxxx@gmail.com>
2023-03-22 15:25:10 +0100

    list changes

--- client/src/components/CalorieList.jsx
+++ client/src/components/CalorieList.jsx
@@ -1,45 +1,42 @@
-import React, { useEffect } from 'react'
-import { DataGrid } from '@mui/x-data-grid';
-import axios from 'axios'
+import React, { useEffect, useState } from "react";
+import { DataGrid } from "@mui/x-data-grid";
+import axios from "axios";
 
 export const CalorieList = () => {
-  
-  const rows = [
-    {id:1,event_time: Date(),item_name: "apple",total_calorie: 123,carbohydrate:10,protein:2,fat:3,amount:81} 
-    
-  ];
-  
+  const [dataGridRows, setDataGridRows] = useState([]);
+
   useEffect(() => {
-    axios.get('http://localhost:8080/api/v1/products/all/20').then(res=>{
-      console.log(res.data);
-      const product = res.data
-      rows.push(product.event_time,product.name,product.totalCalorie,product.carbohydrate,product.protein,product.fat,product.amount)
-      console.log(rows);
-    })
-  }, )
-  
- 
-  
+    axios.get("http://localhost:8080/api/v1/products/all/12").then((res) => {
+      res.data.forEach((element) => {
+        setDataGridRows(...dataGridRows, {
+          id: element.key,
+          date: element.event,
+          name: element.name,
+          totalCalorie: element.totalCalorie,
+          carbohydrate: element.carbohydrate,
+          protein: element.protein,
+          fat: element.fat,
+          amount: element.amount,
+        });
+      });
+    });
+  });
+
   const columns = [
-    { field: 'date', headerName: 'Date', width: 150 },
-    { field: 'name', headerName: 'Product', width: 100 },
-    { field: 'totalCalorie', headerName: 'Calorie', width: 100 },
-    { field: 'carbohydrate', headerName: 'Carbohydrate', width: 100 },
-    { field: 'protein', headerName: 'Protein', width: 100 },
-    { field: 'fat', headerName: 'Fat', width: 100 },
-    { field: 'amount', headerName: 'Amount/Weight', width: 100 },
-
+    { field: "date", headerName: "Date", width: 150 },
+    { field: "name", headerName: "Product", width: 100 },
+    { field: "totalCalorie", headerName: "Calorie", width: 100 },
+    { field: "carbohydrate", headerName: "Carbohydrate", width: 100 },
+    { field: "protein", headerName: "Protein", width: 100 },
+    { field: "fat", headerName: "Fat", width: 100 },
+    { field: "amount", headerName: "Weight", width: 100 },
   ];
-  
-    return (
-      <div className='calorie-list-container'>
-        <div style={{ height: 350, width: '100%' }}>
-          <DataGrid rows={rows} columns={columns} />
-        </div>
+
+  return (
+    <div className="calorie-list-container">
+      <div style={{ height: 350, width: "100%" }}>
+        <DataGrid rows={dataGridRows} columns={columns} />
       </div>
-    );  
-}
-
-
- 
-
+    </div>
+  );
+};
