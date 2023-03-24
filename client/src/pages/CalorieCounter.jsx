b7d5fab4ddc9c7869445402f77be30b1e14e20a7
lukacsdonat03 <lukacsdonatxxx@gmail.com>
2023-03-24 14:49:00 +0100

    counter page kinézetének fixálása

--- client/src/pages/CalorieCounter.jsx
+++ client/src/pages/CalorieCounter.jsx
@@ -1,33 +1,34 @@
-import   { Grid}  from '@mui/material'
-import { Box } from '@mui/system'
-import { CalorieList } from '../components/CalorieList'
-import { Counter } from '../components/Counter'
-import { Footer } from '../components/Footer'
-import { Navbar } from '../components/Navbar'
-
-
+import { Grid } from "@mui/material";
+import { Box } from "@mui/system";
+import { CalorieList } from "../components/CalorieList";
+import { Counter } from "../components/Counter";
+import { Footer } from "../components/Footer";
+import { Navbar } from "../components/Navbar";
+import '../App.css'
 
 export const CalorieCounter = () => {
-  
   return (
-    <div className='Navbar-container'><Navbar></Navbar>
-    <div className='calorie-counter-page-container'>
-      <Box sx={{ flexGrow: 1 }} >
-      <Grid container spacing={2}>
-        <Grid item md={6} sm={6} xs={8}>
-          <CalorieList/>
-        </Grid>
-        <Grid item md={6} sm={6} xs={6} >
-          <Counter/>
-        </Grid>
-        </Grid>
+    <>
+      <div >
+        <Navbar />
+      </div>
+      <div className="calorie-counter-page-container">
+        <Box sx={{ flexGrow: 1 }}>
+          <Grid container spacing={2}>
+            <Grid item md={6} sm={6} xs={8}>
+              <CalorieList />
+            </Grid>
+            <Grid item md={6} sm={6} xs={6}>
+              <Counter />
+            </Grid>
+          </Grid>
         </Box>
-        <div className='footer-container'>
-          <Footer/>
+        <div className="footer-container">
+          <Footer />
         </div>
-    </div>
-    </div>
-  )
-}
+      </div>
+    </>
+  );
+};
 
 export default CalorieCounter;
