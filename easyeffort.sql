d797e6e0cd92b35fbf3714070eae774000aaf323
lukacsdonat03 <lukacsdonatxxx@gmail.com>
2023-03-27 10:01:13 +0200

    db

--- easyeffort.sql
+++ easyeffort.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Gép: 127.0.0.1
--- Létrehozás ideje: 2023. Már 09. 19:23
+-- Létrehozás ideje: 2023. Már 27. 10:00
 -- Kiszolgáló verziója: 10.4.27-MariaDB
 -- PHP verzió: 8.2.0
 
@@ -20,8 +20,6 @@ SET time_zone = "+00:00";
 --
 -- Adatbázis: `easyeffort`
 --
-CREATE DATABASE IF NOT EXISTS `easyeffort` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_hungarian_ci;
-USE `easyeffort`;
 
 -- --------------------------------------------------------
 
@@ -36,8 +34,24 @@ CREATE TABLE `calorie` (
   `carbohydrate` int(11) NOT NULL,
   `protein` double DEFAULT NULL,
   `fat` double DEFAULT NULL,
-  `userId` int(11) NOT NULL
+  `totalCalorie` double NOT NULL,
+  `userId` int(11) NOT NULL,
+  `event_time` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_hungarian_ci;
+
+--
+-- A tábla adatainak kiíratása `calorie`
+--
+
+INSERT INTO `calorie` (`id`, `name`, `amount`, `carbohydrate`, `protein`, `fat`, `totalCalorie`, `userId`, `event_time`) VALUES
+(1, 'muffin', 111, 4, 30.19, 43.97, 533.28, 1, '2023-03-10 17:53:45'),
+(2, 'muffin', 11, 27, 8.4, 25.5, 373.55, 20, '2023-03-10 17:53:45'),
+(5, 'monster', 500, 23, 0, 0, 240, 21, '2023-03-15 14:24:58'),
+(6, 'Milk Chocolate Turkish Delight Thins', 1, 14, 0.7, 4.3, 97.21, 12, '2023-03-15 14:54:11'),
+(7, 'Jelly Bar', 17, 28, 0, 0, 110, 12, '2023-03-16 11:58:42'),
+(8, '1% Milk - 1 cup', 2, 12, 8.53, 2.38, 210.7, 12, '2023-03-18 14:02:31'),
+(9, 'Cheese', 100, 2000, 300, 1000, 18000, 12, '2023-03-19 08:54:56'),
+(10, 'Apple - 1 oz', 2, 8, 0.14, 0.1, 29.48, 12, '2023-03-26 08:09:07');
 
 -- --------------------------------------------------------
 
@@ -70,12 +84,11 @@ CREATE TABLE `user` (
   `id` int(11) NOT NULL,
   `fullname` varchar(50) NOT NULL,
   `email` varchar(50) NOT NULL,
-  `password` varchar(50) NOT NULL,
-  `currentWeight` double DEFAULT NULL,
+  `password` varchar(60) NOT NULL,
+  `currentWeight` double NOT NULL DEFAULT 0,
   `targetWeight` double DEFAULT NULL,
-  `calorieId` int(11) NOT NULL,
-  `currentCalorie` int(11) NOT NULL,
-  `targetCalorie` int(11) DEFAULT NULL,
+  `currentCalorie` int(5) NOT NULL DEFAULT 0,
+  `targetCalorie` double NOT NULL DEFAULT 0,
   `admin` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_hungarian_ci;
 
@@ -83,17 +96,10 @@ CREATE TABLE `user` (
 -- A tábla adatainak kiíratása `user`
 --
 
-INSERT INTO `user` (`id`, `fullname`, `email`, `password`, `currentWeight`, `targetWeight`, `calorieId`, `currentCalorie`, `targetCalorie`, `admin`) VALUES
-(1, 'Teszt Elek', 'tesztelek@tempmail.com', 'admin', NULL, NULL, 0, 0, NULL, 1),
-(2, 'Hű Jenő', 'hujeno@tempmail.com', 'secret', NULL, NULL, 0, 0, NULL, 0),
-(3, 'Mikorka Kálmán', 'kalman@tempmail.com', 'password', NULL, NULL, 0, 0, NULL, 0),
-(12, 'Donat', 'donat@tempmail.com', '$2b$12$pnJk365BIpwmO1vjY6x2Q.ygie9B305FdSW.NoRCDqM', NULL, NULL, 0, 0, NULL, 1),
-(15, 'Donat', 'donat03@tempmail.com', '$2b$12$sWTrAVOeWsJOwA9a67v7H.brWasd5kw0qCjb7PO3L0e', NULL, NULL, 0, 0, NULL, 1),
-(16, 'Dona2t', 'donat02@tempmail.com', '$2b$12$PXFouGzNvcubmFXLK88n5eX9pBteMPcL0nXY5/GC0ft', NULL, NULL, 0, 0, NULL, 1),
-(18, 'Lukács Donát', 'lukacs@gmail.com', '$2b$12$k10mGrbhz5MW1J5vwhZxP.QbGZnIkgoeXTQolFwd0gn', NULL, NULL, 0, 0, NULL, 0),
-(19, 'Lukács Donát', 'asd@gmail.com', '$2b$12$P673bvvNtkZp6X7/fwSbwuUVLrEuUDkHQKaTZactHtP', NULL, NULL, 0, 0, NULL, 0),
-(20, 'Admin', 'admin@admin.com', '$2b$12$3Fu9RDPg.7MuN.w.1vhYa.u4uboOb3qvY/6AGykWPTt', NULL, NULL, 0, 0, NULL, 0),
-(21, 'Alma Kukac', 'alma@gmail.com', '$2b$12$OM8ekVEPkSpM9cTuZ3jRxed8x5xVCTGB0X3qswkXt9Q', NULL, NULL, 0, 0, NULL, 0);
+INSERT INTO `user` (`id`, `fullname`, `email`, `password`, `currentWeight`, `targetWeight`, `currentCalorie`, `targetCalorie`, `admin`) VALUES
+(22, 'Admin', 'admin@admin.com', '$2b$12$yir3Suc0QBrWYmkV2Kb1G.EqfmK6QuOKjNOZZSXJQhNqRA8XPNJ.K', 80, 77, 1189, 2700.52, 1),
+(24, 'Donat', 'donat@gmail.com', '$2b$12$4KcZg3e9sOF.Lw2/JjgLau3vz/KlZkai.Pp1.JuPPyFCsdAG1yvRO', 0, NULL, 0, 0, 0),
+(26, 'asd', 'asd@asd.com', '$2b$12$IKC0.nqeGYWbyVfob4img.By3Vpek8VnrvR/x.Kj9x1HG.46GgSBe', 0, NULL, 0, 0, 0);
 
 --
 -- Indexek a kiírt táblákhoz
@@ -104,7 +110,8 @@ INSERT INTO `user` (`id`, `fullname`, `email`, `password`, `currentWeight`, `tar
 --
 ALTER TABLE `calorie`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `FK_calorieUser` (`userId`);
+  ADD KEY `FK_calorieUser` (`userId`),
+  ADD KEY `event_timestamp` (`event_time`);
 
 --
 -- A tábla indexei `comment`
@@ -128,7 +135,7 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT a táblához `calorie`
 --
 ALTER TABLE `calorie`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT a táblához `comment`
@@ -140,7 +147,7 @@ ALTER TABLE `comment`
 -- AUTO_INCREMENT a táblához `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
