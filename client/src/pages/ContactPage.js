ea884b425bf954e396f7440801820ec18d878cd3
Lukács Donát <lukacsdonatxxx@gmail.com>
2023-03-28 11:01:08 +0200

    contact page

--- client/src/pages/ContactPage.js
+++ client/src/pages/ContactPage.js
@@ -1,10 +1,22 @@
-import React from "react";
-import "./Contact.css";
+import React, { useEffect, useState } from "react";
 import { Footer } from "../components/Footer";
 import { Navbar } from "../components/Navbar";
 import { Typography } from "@mui/material";
+import axios from "axios";
+import "./Contact.css";
 
-const ContactPage = () => {
+ const ContactPage = () => {
+  const [formData, setFormData] = useState({})
+
+  const handleSubmit = (e)=>{
+    //axios.post('http://localhost:8080/api/v1/contact/messages',
+      //{subject:formData.subject,message:formData.message},
+      //{headers:{'Content-Type':'application/json'},withCredentials:true}
+    //)
+    e.preventDefault();
+    console.log('hello');
+  }
+
   return (
     <>
       <Navbar />
@@ -24,7 +36,7 @@ const ContactPage = () => {
           <label htmlFor="message">Message</label>
           <textarea id="message" name="message" rows="7" />
           <br />
-          <button type="submit">Send</button>
+          <button onClick={(e)=>handleSubmit}>Send</button>
         </form>
         <div className="bottom-footer-container">
           <Footer />
@@ -33,5 +45,4 @@ const ContactPage = () => {
     </>
   );
 };
-
-export default ContactPage;
+ export default ContactPage
