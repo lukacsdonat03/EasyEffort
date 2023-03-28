c967ae5c52d429298be8ebde6903cd49cb197a13
Lukács Donát <lukacsdonatxxx@gmail.com>
2023-03-28 10:09:08 +0200

    contact page változtatások

--- client/src/pages/ContactPage.js
+++ client/src/pages/ContactPage.js
@@ -1,30 +1,37 @@
-import React from 'react'
-import "./Contact.css"
-import { Footer } from '../components/Footer';
-import { Navbar } from '../components/Navbar'
+import React from "react";
+import "./Contact.css";
+import { Footer } from "../components/Footer";
+import { Navbar } from "../components/Navbar";
+import { Typography } from "@mui/material";
 
 const ContactPage = () => {
   return (
-    <div>
-    <Navbar></Navbar>
-    <div>
-      <h1>Contact Us</h1>
-      <form>
-        <label htmlFor="name">Name</label>
-        <input type="text" id="name" name="name" />
-        <br />
-        <label htmlFor="email">Email</label>
-        <input type="email" id="email" name="email" />
-        <br />
-        <label htmlFor="message">Message</label>
-        <textarea id="message" name="message" rows="5" />
-        <br />
-        <button type="submit">Send</button>
-      </form>
-      <Footer/>
-    </div>
-    </div>
-  )
-}
+    <>
+      <Navbar />
+      <div>
+        <h1>Contact Us</h1>
+        <Typography
+          align="center"
+          variant="h5"
+          fontFamily={"Tilt Warp"}
+          fontStyle={"italic"}
+        >
+          If you have any error or propblem contact with our admins:{" "}
+        </Typography>
+        <form>
+          <label htmlFor="subject">Subject</label>
+          <input type={"text"} name="subject" />
+          <label htmlFor="message">Message</label>
+          <textarea id="message" name="message" rows="7" />
+          <br />
+          <button type="submit">Send</button>
+        </form>
+        <div className="bottom-footer-container">
+          <Footer />
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default ContactPage
+export default ContactPage;
