36b75507829c212190b56cad7d6cb78933369717
lukacsdonat03 <lukacsdonatxxx@gmail.com>
2023-04-09 11:53:26 +0200

    Footer linkek

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,14 +1,28 @@
+import { Popover, Typography } from "@mui/material";
 import React from "react";
 import { Link } from "react-router-dom";
 
 export const Footer = () => {
+  const [anchorEl, setAnchorEl] = React.useState(null);
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const open = Boolean(anchorEl);
+  const id = open ? "simple-popover" : undefined;
+
   return (
     <footer className="bg-dark text-center text-white">
       <div className="p-4 pb-0">
         <section className="mb-4">
           <Link
             className="btn btn-outline-light btn-floating m-1"
-            to="/"
+            to="/about"
             role="button"
           >
             About
@@ -16,7 +30,7 @@ export const Footer = () => {
 
           <Link
             className="btn btn-outline-light btn-floating m-1"
-            to="/"
+            to="/contacts"
             role="button"
           >
             Contacts
@@ -24,7 +38,7 @@ export const Footer = () => {
 
           <Link
             className="btn btn-outline-light btn-floating m-1"
-            to="/"
+            to="https://www.facebook.com/donat.lukacs.5"
             role="button"
           >
             Facebook
@@ -34,12 +48,26 @@ export const Footer = () => {
             className="btn btn-outline-light btn-floating m-1"
             to="/"
             role="button"
+            aria-describedby={id}
+            onClick={handleClick}
           >
             Tel
+            <Popover
+              id={id}
+              open={open}
+              anchorEl={anchorEl}
+              onClose={handleClose}
+              anchorOrigin={{
+                vertical: "bottom",
+                horizontal: "left",
+              }}
+            >
+              <Typography  sx={{ p: 2 }}>+36 20 524 6903</Typography>
+            </Popover>
           </Link>
           <Link
             className="btn btn-outline-light btn-floating m-1"
-            to="/"
+            to="https://github.com/lukacsdonat03"
             role="button"
           >
             Github
